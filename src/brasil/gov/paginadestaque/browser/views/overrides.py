4d0b6c1a824826257192623bb5508883f778ad4c
idgserpro <idg@serpro.gov.br>
2017-08-02 09:38:01 -0300

    alterado registro da view do tipo capa para zcml

--- src/brasil/gov/paginadestaque/browser/views/overrides.py
+++ src/brasil/gov/paginadestaque/browser/views/overrides.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_inner
 from brasil.gov.paginadestaque.behaviors.interfaces import IBackgroundImage
-from brasil.gov.paginadestaque.interfaces import IBrowserLayer
 from collective.cover.browser.cover import View as CoverBaseView
-from collective.cover.interfaces import ICover
 from five import grok
 from plone.memoize import view
 
@@ -13,12 +11,6 @@ grok.templatedir('templates')
 class CoverOverridesView(CoverBaseView):
 
     """Overrides collective.cover default view."""
-
-    grok.context(ICover)
-    grok.layer(IBrowserLayer)
-    grok.name('view')
-    grok.require('zope2.View')
-    grok.template('cover_overrides_view')
 
     @view.memoize
     def background(self):
